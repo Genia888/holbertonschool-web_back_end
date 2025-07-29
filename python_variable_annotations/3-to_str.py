e241d0f8092f301aded3707ef352c45f56513aa8
Genia888 <khalepo@gmail.com>
2025-07-29 11:03:26 +0200

    Add main script to test to_str function for float to string conversion

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module for converting a float to a string with type annotations."""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
