b09ecfe6b5b844c2fbaded21ca8348d6b09e1c8f
Genia888 <khalepo@gmail.com>
2025-07-29 10:57:21 +0200

    Add floor function module with type annotations and main script for testing

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A module for concatenating two strings with type annotations."""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of a float as an integer."""
+    return math.floor(n)
