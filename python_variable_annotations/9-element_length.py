2abdc1af70a7ce8e2628372e2d80548f4eb6a5fc
Genia888 <khalepo@gmail.com>
2025-07-29 12:07:42 +0200

    Add element_length function to return lengths of elements in a list

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""That defines a function to return the length of each element in a list."""
+
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
