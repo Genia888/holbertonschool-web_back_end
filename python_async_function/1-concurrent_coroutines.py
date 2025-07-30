6300959dec9ad6bdeac1d9e62c370a71bffcd71e
Genia888 <khalepo@gmail.com>
2025-07-30 15:34:57 +0200

    Add wait_n coroutine and main script for testing

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"Module that defines the wait_n coroutine"
+
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    delays = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await d for d in asyncio.as_completed(delays)]
