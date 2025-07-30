b0c8e18bb14cdfd64a5fc44ae7e70ae9584064db
Genia888 <khalepo@gmail.com>
2025-07-30 22:31:10 +0200

    Add async_generator and main script for asynchronous value printing

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module that defines the corountine will loop 10 times"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Asynchronously"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
