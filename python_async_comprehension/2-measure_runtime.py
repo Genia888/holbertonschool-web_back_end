b85500ee816d55e431f86730ba27b378e9e95443
Genia888 <khalepo@gmail.com>
2025-07-31 00:03:07 +0200

    Refactor measure_runtime to use time.perf_counter and simplify async_comprehension calls

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -9,12 +9,7 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 async def measure_runtime() -> float:
     """Measures the runtime of four async_comprehension calls."""
-    start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-    end = time.time()
+    start = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end = time.perf_counter()
     return end - start
