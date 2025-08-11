3b9a2b969e03fd9fb38119f481a272c8d7c71fff
Genia888 <khalepo@gmail.com>
2025-08-11 15:18:24 +0200

    Refactor index_range function to specify return type and simplify calculations

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,8 +2,6 @@
 """Simple helper function for pagination"""
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> tuple[int, int]:
     """Return a tuple of start and end indexes for a given page"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+    return (page - 1) * page_size, page * page_size
