af84b06f289ba883d5b1d8ae9f6e8ac3d8591005
Genia888 <khalepo@gmail.com>
2025-09-01 14:58:50 +0200

    Implement initial versions of read_file and read_file_async modules, and update main files to utilize them

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on("data", (data) => {
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 });
-process.stdin.on("end", () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
