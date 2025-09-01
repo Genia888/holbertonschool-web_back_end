2b0b32a87c76c6a8b474d8ef9b5d0bfb248a80ec
Genia888 <khalepo@gmail.com>
2025-09-01 15:02:29 +0200

    Add a blank line for improved readability in stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,6 +3,7 @@ process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
 });
+
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
